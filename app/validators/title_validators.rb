78acc19cd5d0e5e9d7365d2a1401aa9210046221
ivnicolas <ikennanicolas@gmail.com>
2021-04-27 20:05:27 -0400

    commit tree eff6d9d7f1f2e1d5e6cc262a5f2f85f62858cc18

---
+++ app/validators/title_validators.rb
@@ -0,0 +1,7 @@
+class TitleValidator < ActiveModel::Validator
+    # def validate (record)
+    #     unless record.title.include("Wont Believe" || "Secret" || "Guess" || "Top")
+    #         record.errors[:title] << "This post is no click-baity!"
+    #     end 
+    # end 
+end 
